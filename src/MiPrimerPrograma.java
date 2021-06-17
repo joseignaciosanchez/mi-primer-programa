e5e134d78d7a5e4933dffa63c78aebde4cf66e31
Your Name <you@example.com>
2021-06-17 12:17:53 -0400

    ...

---
+++ src/MiPrimerPrograma.java
@@ -0,0 +1,16 @@
+
+public class MiPrimerPrograma {
+
+	public static void main(String[] args) {
+		String nombre = "Coding Dojo";
+		System.out.println("Mi nombre es "+nombre);
+		
+		int edad = 100;
+		System.out.println("Tengo "+edad+" años de edad.");
+		
+		String ciudad = "Burbank, CA";
+		System.out.println("Mi ciudad es "+ciudad);
+
+	}
+
+}
